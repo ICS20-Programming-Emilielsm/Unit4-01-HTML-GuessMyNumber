956487d3d85a3793eaa898b56de9f6909d360d07
Emilie LSM <99273880+emilie06lsm@users.noreply.github.com>
2022-04-27 15:06:07 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-// Copyright (c) 2022 Ms Raffin All rights reserved
-//
-// Created by: Ms Raffin
-// Created on: March 2022
-// This file contains the JS functions for index.html
 
 "use strict"
 
@@ -13,13 +8,18 @@ function displayNumber () {
   // initialize variables
 	let answer = ""
 	
-	// get user input
+// get user input
 	let guess = parseInt(document.getElementById('guess').value)
+  let randomNumber = Math.floor(Math.random() * 6) + 1
 
 
-  // determine the greeting based on the hour of the day in 24-hour format
-	if (guess == Math.floor((Math.random() * 6) + 1)) {
-		answer = "Correct!"
+  	// if guess is right says correct
+	if (guess == randomNumber){
+		answer = "CORRECT!!!"
+	} 
+	//if not says incorrect	
+	else {
+		answer = "INCORRECT. The correct answer was " + randomNumber + "."
 	}
 
   // display the results
